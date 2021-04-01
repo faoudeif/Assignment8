547bb8825dd754cb29cc035f24e850080e822987
faoudeif <77484659+faoudeif@users.noreply.github.com>
2021-04-01 18:15:02 -0400

    Update clearNames.php

--- clearNames.php
+++ clearNames.php
@@ -1,3 +1,30 @@
 <?php
+require_once '../classes/Pdo_methods.php';
+
+$pdo = new PdoMethods();
+
+$sql = "DELETE FROM names WHERE name = :name";
+				
+$bindings = [
+    [':name', $name, 'str'],
+];
+
+
+$records = $pdo->otherBinded($sql, $bindings);
+
+if($records === "error") {
+    $response = (object) [
+        'masterstatus' => 'error',
+        'msg' => 'Could not delete names'
+    ];
+    echo json_encode($response);
+} else {
+    $response = (object) [
+        'masterstatus' => 'success',
+        'names' => 'All names deleted'
+    ];
+    echo json_encode($response);
+}
+
 
 ?>

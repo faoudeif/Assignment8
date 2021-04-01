c41d0b2b396f3dba52e674a08c1442113cb9791e
faoudeif <77484659+faoudeif@users.noreply.github.com>
2021-04-01 18:15:26 -0400

    Update displayNames.php

--- displayNames.php
+++ displayNames.php
@@ -1,3 +1,31 @@
 <?php
+require_once '../classes/Pdo_methods';
+
+$pdo = new PdoMethods();
+
+$sql = "SELECT * FROM names";
+
+$records = $pdo->selectNotBinded($sql);
+
+if($records === "error") {
+    $response = (object) [
+        'masterstatus' => 'error',
+        'msg' => 'Could not display names'
+    ];
+    echo json_encode($response);
+} else {
+    function output($records){
+		$list = '<ol>';
+		foreach ($records as $row){
+			$list .= "<li>{$row['name']}</li>";
+		}
+		$list .= '</ol>';
+	}
+    $response = (object) [
+        'masterstatus' => 'success',
+        'names' => $list
+    ];
+    echo json_encode($response);
+}
 
 ?>

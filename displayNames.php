eb6828474661224827223b11ef981c9946438339
faoudeif <77484659+faoudeif@users.noreply.github.com>
2021-04-04 22:50:52 -0400

    Update displayNames.php

--- displayNames.php
+++ displayNames.php
@@ -3,18 +3,21 @@ require_once '../classes/Pdo_methods.php';
 
 $pdo = new PdoMethods();
 
-$sql = "SELECT * FROM names";
+$sql = "SELECT * FROM names ORDER BY name";
 
 $records = $pdo->selectNotBinded($sql);
 
+$names = "<p>";
 foreach($records as $row) {
-    $names = "<p>" . $row['name'] . "</p>";
+$names .= "<p>" . $row['name'] . "</p>";
 }
+$names .= "</p>";
 
 $response = (object) [
     'masterstatus' => 'success',
     'names' => $names
 ];
+
 echo json_encode($response);
 
 ?>
